274f3989f842f3a3bac26b0519da0ff2d1515a36
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-07-23 01:42:54 +0700

    JS-Task-03

---
+++ JavaScript 03/Nomor 4.js
@@ -0,0 +1,16 @@
+/*
+======= No. 4 =======
+Buatlah program menggunakan method fetch untuk menampilkan semua name (hanya name nya saja) dari REST API dibawah ini
+https://jsonplaceholder.typicode.com/users
+Gunakan debugger console bawaan browser Chrome untuk melihat hasilnya
+*/
+
+let base_url = "https://jsonplaceholder.typicode.com/users"
+const getName = () => fetch(`${base_url}`)
+
+getName()
+    .then(postResponse => postResponse.json())
+    .then(postResult => postResult.map((item) => {
+        console.log(item.name)
+    })
+    )
