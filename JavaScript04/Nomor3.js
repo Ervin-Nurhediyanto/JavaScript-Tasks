181d76f7a9850a0d306421b978cb924d34f921fd
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-07-27 18:27:03 +0700

    JS-Task-04-Edit No.3

--- JavaScript04/Nomor3.js
+++ JavaScript04/Nomor3.js
@@ -1,4 +1,5 @@
-/*Buatlah Algoritma, Flowchart dan Code untuk soal dibawah ini:
+/*
+3. Buatlah Algoritma, Flowchart dan Code untuk soal dibawah ini:
 
 Diberikan sebuah variabel yang berisikan bilangan integer dengan ketentuan angka 0 (nol) dalam variabel tersebut merupakan pemisah antara satu bilangan dengan bilangan lainnya. Bilangan-bilangan tersebut akan dipisah dan diurutkan berdasarkan angka di bilangan-bilangan itu sendiri. Setelah itu, bilangan hasil pengurutan akan digabung kembali dengan tanpa pemisah dengan output berupa bilangan integer. Buatlah method/function yang menerima parameter hanya deret angka dan menghasilkan output seperti keterangan di atas.
 */
@@ -7,16 +8,19 @@ const divideAndSort = (number) => {
     const str = number.toString().split('0')
     const map = str.map((item) => {
         return Array.from(item).sort((a, b) => a - b).join('')
-
     })
     const res = Number(map.join(''))
 
     console.log(str)
+    console.log(map)
     console.log(res)
 
 }
 
 divideAndSort(5956560159466056);
+
+// Array.from(item)                         => [ ['5','9','5','6','5','6'], ['1','5','9','4','6','6'], ['5','6'] ]
+// Array.from(item).sort((a, b) => a - b)   => [ ['5','5','5','6','6','9'], ['1','4','5','6','6','9'], ['5','6'] ]
 
 /*
 === Algoritma ===
